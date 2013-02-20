c36529e1990890d4dc279e71401c8a0448b825b2
fasteddie31003 <ejanderson4@gmail.com>
2013-02-20 11:53:54 -0600

    first commit

---
+++ js/expense.js
@@ -0,0 +1,149 @@
+//objectComment.commentCollection.localStorage= new Backbone.LocalStorage("commentsLocalStorage")
+
+var Transaction = Backbone.Model.extend({
+	name:'transaction',
+	localStorage: new Backbone.LocalStorage("transactions")
+})
+
+var TransactionCollection = Backbone.Collection.extend({
+	localStorage: new Backbone.LocalStorage("transactions"),
+	model:Transaction
+})
+
+var TransactionRow= Backbone.View.extend({
+	initialize:function(){
+
+		this.render()
+	},
+	tagName: 'tr',
+	events:{
+		'click .destory':'destory'
+	},
+	render:function(){
+		var color= (this.model.get('type')=='expense')?'red':'black'
+		var html = _.template('<td><%= type %></td><td><%= description %></td><td><%= category %></td><td style="color:'+color+'"><%= amount %></td><td><button class="btn btn-danger btn-mini destory">delete</button></td>',this.model.attributes)
+		$(this.el).html(html)
+	},
+	destory:function(){
+		this.options.collection.remove(this.model)
+		this.model.destroy()
+		this.options.listView.render()
+	}
+})
+
+var TransactionView= Backbone.View.extend({
+	initialize:function(){
+
+		this.render()
+	},
+	events:{
+
+	},
+	render:function(){
+		var html = _.template($('#modalForm').html(),this.model.attributes)
+		$(this.el).html(html)
+	},
+	submit:function(){
+		var values=$(this.el).find('form').serializeObject()
+		values.amount=parseInt(values.amount,10)
+		this.model.set(values).save()
+	}
+})
+
+var ListView=Backbone.View.extend({
+	initialize:function(){
+
+		this.render()
+	},
+	events:{
+		'click .addButton':'addTransaction'
+	},
+	render:function(){
+		var self =this
+		var html = $(_.template($('#listView').html(),{}))
+		this.options.transactionCollection.each(function(model){
+			var element= $('<tr></tr>')
+			var row = new  TransactionRow({
+				el:element,
+				model:model,
+				listView:self,
+				collection:self.options.transactionCollection
+			})
+			html.find('tbody').append(element)
+		})
+		html.find('.totals').text('$'+this.findTotal())
+		$(this.el).html(html)
+		
+	},
+	addTransaction:function(){
+		var self = this
+		var footer=$('<div><a href="#" class="btn add btn-primary" data-dismiss="modal">Save</a><a href="#" class="btn" data-dismiss="modal">Close</a></div>');
+		var html=$('<div></div>')
+		var modal=showModalView(html,'Add Transaction',footer)
+		var model = new Transaction()
+		
+		var transactionView=new TransactionView({
+			el:html,
+			model:model
+		})
+		
+		footer.find('.add').click(function(){
+			transactionView.submit()
+			self.options.transactionCollection.add(model)
+			self.render()
+		})
+
+	},
+	findTotal:function(){
+		var value = this.options.transactionCollection.reduce(function(num,trans){
+			var value
+			if(trans.get('type')=='expense'){
+				value=num-trans.get('amount')
+			}else{
+				value=trans.get('amount')+num
+			}
+
+			return value
+			
+		},0)
+		return value
+	}
+});
+
+
+var showModalView= function(body,header,footer,optionsList){
+
+	var options = optionsList||{}
+
+	header=header?header:'';
+	footer=footer?footer:'<a href="#" class="btn" data-dismiss="modal">Close</a>';
+	var modal = $(_.template( $("#modal_template").html(), {header:header }  ));
+	modal.find('.footer').html(footer);
+	modal.find('.modalBody').html(body);
+	
+
+	$(modal).modal({backdrop:true,
+					hide:function(){
+						$(this).remove();
+					}});
+
+	return modal;
+}
+
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
